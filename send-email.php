8412fb2e2e476d6cb53e1b422d3c4e3a0a08086f
GerbenKG <165113786+GerbenKG@users.noreply.github.com>
2025-09-23 16:53:38 +0200

    commit

---
+++ send-email.php
@@ -0,0 +1,26 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "autoload.php"
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host= "smtp.example.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port= 587;
+
+$mail->Username = "";
+$mail->Password= "";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("g.e.dekoninggans@gmail.com");
